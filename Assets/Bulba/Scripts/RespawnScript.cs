7fe8724c1c38ff75524b2b12fdbd52cd7b8ecec0
almostblakbabu <sushant.bhonde@gmail.com>
2021-11-24 15:33:08 +0100

    Sushant's Commit. readded grapple stop hooking after dead

--- Assets/Bulba/Scripts/RespawnScript.cs
+++ Assets/Bulba/Scripts/RespawnScript.cs
@@ -6,12 +6,14 @@ public class RespawnScript : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
+    [SerializeField] private GrapplingGun grapplingGun;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             player.transform.position = respawnPoint.transform.position;
+            grapplingGun.StopGrapple();
             Physics.SyncTransforms();
             Debug.Log("PLAYER IS DEAD");
         }
